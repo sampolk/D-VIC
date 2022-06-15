872bc47fe62c9bc2fbfcefd9c2983f4a47274e00
jasoncui <kangnicui2@gmail.com>
2022-06-15 14:28:58 +0800

    Delete backEnd/clusteringMethods/competingMethods/DPCDLP directory

--- backEnd/clusteringMethods/competingMethods/DPCDLP/calculatePP.m
+++
@@ -1,9 +0,0 @@
-function [PP,WW]=calculatePP(W,neighbs)
-    n=size(W,1);
-    b=0:n:(n*n-1);
-    WW=zeros(n);
-    neighbs=neighbs+b';
-    WW(neighbs)=1;
-    WW=WW'.*W;
-    PP=WW./sum(WW,2);
-end
--- backEnd/clusteringMethods/competingMethods/DPCDLP/neighborhood.m
+++
@@ -1,5 +0,0 @@
-function [disFromNeighb,neighbs]=neighborhood(X,k)
-    [neighbs,disFromNeighb]=knnsearch(X,X,'k',k+1);
-    neighbs(:,1)=[];
-    disFromNeighb(:,1)=[];
-end
