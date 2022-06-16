2aae727a068b1d2d827cb0165bfea3dab49a0a5e
jasoncui <kangnicui2@gmail.com>
2022-06-16 22:15:59 +0800

    Add files via upload

--- backEnd/clusteringMethods/competingMethods/PGDPC/PGDPCGS.m
+++ backEnd/clusteringMethods/competingMethods/PGDPC/PGDPCGS.m
@@ -1,1 +1,60 @@
+%% PGDPC
+% Extracts performances for DPC-DLP
 
+%% Grid Search Parameters
+clear;
+clc;
+
+% Set parameters of PGDPC
+NNs = [unique(round(10.^(1:0.1:2.7),-1)), 600, 700, 800, 900];
+
+%%
+
+datasets = {'SalinasACorrected',  'JasperRidge','PaviaCenterSubset2','IndianPinesCorrected',  'syntheticHSI5149Stretched'};
+datasetNames = {'Salinas A',      'Jasper Ridge',  'Pavia Subset',    'Indian Pines',           'Synthetic HSI'};
+
+for dataIdx =  [1]
+    
+    if dataIdx == 5
+        % Set number of nearest neighbors to use in graph and KDE construction.
+        NNs = [unique(round(10.^(1:0.1:2.7),-1)), 600, 700, 800, 900];
+    end
+    
+
+    % ===================== Load and Preprocess Data ======================
+    [X,M,N,D,HSI,GT,Y,n, K] = loadHSI(datasetNames{dataIdx});
+
+    % ============================== DVIS ==============================
+ 
+    % Preallocate memory
+    maxOA = 0;
+    OAs     = NaN*zeros(length(NNs),1);
+    kappas  = NaN*zeros(length(NNs),1);
+    Cs      = zeros(M*N,length(NNs),1);
+
+    delete(gcp('nocreate'))
+    poolObj = parpool;
+
+    currentPerf = 0;
+    % Run Grid Searches
+    for i = 1:length(NNs)
+
+        if poolObj.NumWorkers<6
+            delete(gcp('nocreate'));
+            poolObj = parpool;
+        end
+        
+        C = PGDPC(X,NNs(i),K);
+        [ OAs(i), kappas(i), tIdx] = calcAccuracy(Y, C, ~strcmp('JasperRidge', datasets{dataIdx}));
+        Cs(:,i) = C;
+
+        disp(['PGDPC: '])
+        disp([i/length(NNs), maxOA])
+        
+        maxOA = max(mean(OAs,3), [],'all');
+
+        save(strcat('PGDPCResults', datasets{dataIdx}),  'OAs', 'kappas', 'Cs', 'NNs', 'maxOA')
+        
+    end
+
+end
---
+++ backEnd/clusteringMethods/competingMethods/PGDPC/README.md
@@ -0,0 +1,6 @@
+# Peak-graph-based-Fast-Density-Peak-Clustering-for-Image-Segmentation
+We proposed an fast DPC-based clustering algorithm (PGDPC) for image segmentation which is published in IEEE SIGNAL PROCESSING LETTERS, 2021. Doi: 10.1109/LSP.2021.3072794
+The proposed algorithm has three contributions
+1) PGDPC with time complexity O(nlog(n)) can handle large datasets rapidly; 
+2) the peak-graph-based allocation strategy can integrally reconstruct the spatial information of complex-shaped clusters; 
+3) PGDPC is found superior to some popular state-of-the-art methods when applied to real image segmentation.
